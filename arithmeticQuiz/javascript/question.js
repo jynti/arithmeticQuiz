9015d26ad8e8d457269f4b511cc16c4aa59d8abb
jynti <33417806+jynti@users.noreply.github.com>
2017-12-27 19:24:07 +0530

    Update and rename page_in_quiz.js to question.js

---
+++ arithmeticQuiz/javascript/question.js
@@ -0,0 +1,52 @@
+function Question(questionElements, details, quiz) {
+  this.questionField = questionElements.questionField;
+  this.inputAnswer = questionElements.inputAnswer;
+  this.form = questionElements.form;
+
+  this.totalQuestion = details.totalQuestion;
+  this.allOperators = details.operators;
+  this.correctScore = details.correctScore;
+  this.incorrectScore = details.incorrectScore;
+
+  this.quiz = quiz;
+}
+
+Question.prototype.init = function() {
+  this.createQuestion();
+  this.calculateCorrectAns();
+}
+
+Question.prototype.createRandomNumbers = function() {
+  return Math.floor(Math.random() * this.totalQuestion);
+};
+
+Question.prototype.createRandomOperator = function() {
+  var randomNumber = Math.floor(Math.random() * this.allOperators.length);
+  return this.allOperators[randomNumber];
+};
+
+Question.prototype.createQuestion = function() {
+  this.randomNum1 = this.createRandomNumbers();
+  this.randomNum2 = this.createRandomNumbers();
+  this.operator = this.createRandomOperator();
+  var questionText = "What is " + this.randomNum1 + " " + this.operator + " " + this.randomNum2 + " ?";
+  this.questionField.text(questionText);
+};
+
+Question.prototype.calculateCorrectAns = function() {
+  this.correctAns = Math.floor(eval("this.randomNum1" + this.operator  + "this.randomNum2"));
+};
+
+Question.prototype.calculateScore = function(){
+  var presentScoreText = "Your score for the previous question is: ";
+  this.userAnswer = this.inputAnswer.val();
+  if(this.userAnswer == this.correctAns) {
+    presentScoreText += this.correctScore;
+    this.quiz.totalScore += this.correctScore;
+  } else {
+    presentScoreText += this.incorrectScore;
+    this.quiz.incorrectQuestions.push(this);
+  }
+  return presentScoreText;
+}
+
