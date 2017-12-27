1adbec114ecef336ce61d40102551b482899a746
jynti <33417806+jynti@users.noreply.github.com>
2017-12-27 19:25:03 +0530

    Update and rename quiz.js to arithmetic_quiz.js

---
+++ arithmeticQuiz/javascript/arithmetic_quiz.js
@@ -0,0 +1,81 @@
+function ArithmeticQuiz(formElements, details) {
+  this.incorrectQuestions = [];
+  this.container = formElements.container;
+  this.form = formElements.form;
+  this.questionField = formElements.question;
+  this.inputAnswer = formElements.inputAnswer;
+  this.nextButton = formElements.nextButton;
+  this.count = 0;
+  this.totalScore = 0;
+  this.details = details;
+  this.totalQuestion = details.totalQuestion;
+}
+
+ArithmeticQuiz.prototype.init = function() {
+  this.form.append($("<p></p>"));
+  this.createQuestion();
+  this.bindClickToNext();
+};
+
+ArithmeticQuiz.prototype.createQuestion = function() {
+  var questionElements = {
+    questionField: this.questionField,
+    inputAnswer: this.inputAnswer,
+    form: this.form
+  }
+  this.question = new Question(questionElements, this.details, this);
+  this.question.init();
+};
+
+ArithmeticQuiz.prototype.bindClickToNext = function() {
+  var _this = this;
+  this.nextButton.on("click", function() {
+    var scoreOutput = _this.question.calculateScore();
+    if (++_this.count == _this.totalQuestion) {
+      _this.lastQuestion();
+      _this.form.remove();
+    } else {
+      _this.form.find("p").empty().append(scoreOutput);
+      _this.createQuestion();
+      _this.inputAnswer.val("");
+    }
+  });
+};
+
+ArithmeticQuiz.prototype.lastQuestion = function() {
+  var _this = this;
+  var scorePara = $("<p></p>").text("Your total score is " + this.totalScore);
+  this.container.append(scorePara);
+  var wrongAnswersList = $("<ol></ol>");
+  this.incorrectQuestions.forEach(function(question, index) {
+    _this.incorrectAnsweredListCreator(wrongAnswersList, question);
+  });
+  this.container.append(wrongAnswersList);
+};
+
+ArithmeticQuiz.prototype.incorrectAnsweredListCreator = function(wrongAnswersList, question) {
+  var listItem = $("<li></li>");
+  var divInsideList = $("<div></div>");
+  listItem.append(divInsideList);
+  var ques = "What is " + question.randomNum1 + " " + question.operator + " " + question.randomNum2 + "?";
+  divInsideList.html("Question: " + ques + "<br>" + "Correct Ans: " + question.correctAns + "<br>" + "Your answer: " + question.userAnswer + "<br>");
+  wrongAnswersList.append(listItem);
+}
+
+$(document).ready(function() {
+  var formElements = {
+    container: $("[data-type='container']"),
+    form: $("[data-type='form']"),
+    question: $("[data-type='question']"),
+    inputAnswer: $("[data-type='answer']"),
+    nextButton: $("[data-type='button']")
+  };
+  var details = {
+    operators: ['+', '-', '*', '/'],
+    totalQuestion: 20,
+    correctScore: 2,
+    incorrectScore: 0
+  };
+  var arithmeticQuiz = new ArithmeticQuiz(formElements, details);
+  arithmeticQuiz.init();
+});
